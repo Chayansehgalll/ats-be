9b2d9b5fe7d6ccb39b0218dcbb30e197a0d0f75a
Chayan Sehgal <chayansehgal2971@gmail.com>
2026-04-17 01:12:33 +0530

    fix port issue

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 const pdfParse = require("pdf-parse");
 const mammoth = require("mammoth"); // For DOCX parsing
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 app.use(cors({
   origin: "*"
